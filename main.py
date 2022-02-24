5fb8a9c36c74b682d2d475dc17f52788cd886eb3
artemisIsNotBeam <66878297+artemisIsNotBeam@users.noreply.github.com>
2022-02-23 18:05:09 -0800

    my own that works

--- main.py
+++ main.py
@@ -1,42 +1,26 @@
 import pandas as pd
 from PIL import Image, ImageDraw
 
-url = 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=text&starttime=1900-01-01&minmagnitude=8'
 
-myData = pd.read_csv(url,sep="|")
-theMap = Image.open("world-map.png")
-drawCtx = ImageDraw.Draw(theMap)
-width,height=theMap.size
+url = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=text&starttime=1900-01-01&minmagnitude=8"
+myData = pd.read_csv(url,sep ="|")
+map = Image.open("world-map.png")
+mapctx = ImageDraw.Draw(map)
+width,height = map.size
 
-mid_width = width //2 + 1
-mid_height = height //2 - 1
-
-print(width)
-print(height)
-#myList = ["Amy","Keshav","Andy","Owen","Aries","Calista","Mr. Russ","Amanda"]
-
+mid_width = width//2
+mid_height = height//2
 for idx,item in myData.iterrows():
-    x = mid_width + item["Longitude"] / 10 * 32.25
-    y = mid_height - item["Latitude"] / 10 * 32.25
-    drawCtx.pieslice([x - 16, y - 16, x + 16, y + 16], 240, 300, fill="blue")
-
-theMap.save("quakeImage.png")
-  
-
-
+	x = mid_width + item["Longitude"] / 10 * 32.25
+	y = mid_height - item["Latitude"] / 10 * 32.25
+	mapctx.pieslice([x - 16, y - 16, x + 16, y + 16], 240, 300, fill="blue")
+	
 '''
-print(len(myData.index))
-
-# opens the image to be drawn
 myImage = Image.open("blankCanvas.png")
-# "Get Context"
 draw = ImageDraw.Draw(myImage)
 
-draw.rectangle([0,0,100,100],fill="green",outline="red",width=4)
-draw.ellipse([200,250,300,350], fill="yellow")
-myImage.save("newImage.png")
+draw.rectangle([0,0,100,100], fill="red")
+draw.line([5,10,80,100], width = 10, fill="green")
 '''
- #draw.ellipse((25, 50, 175, 200), fill="red")
 
-
-
+map.save("quakeImage.png")
